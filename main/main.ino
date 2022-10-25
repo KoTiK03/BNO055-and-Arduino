0e0a044c62013ac277e5b2569a8ab168bf13e866
KoTiK03 <ALEUA2010@gmail.com>
2022-10-25 19:48:30 +0300

    Create main.ino

---
+++ main/main.ino
@@ -0,0 +1,23 @@
+//Данная програма должна будет собирать данные с BNO055
+
+//Константы
+#define BAUDRATE 115200
+//Библиотеки
+#include <Wire.h>
+#include <Adafruit_Sensor.h>      //Библиотека для работы с разного рода датчиками всякими
+#include <Adafruit_BNO055.h>      //Её производная 
+#include <utility/imumaths.h>
+
+//Переменные
+int i;
+
+void setup() {
+  
+
+
+}
+
+void loop() {
+
+
+}
